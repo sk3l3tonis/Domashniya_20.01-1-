0df82940b30a17c7886ef28f4ec1e8977c5a1baf
sk3l3tonis <kostik63737@gmail.com>
2024-02-10 10:56:38 +0300

    Добавьте файлы проекта.

---
+++ DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using DateAndTimeLib;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime firstDate = new DateTime(2022, 1, 1);
+        DateTime secondDate = new DateTime(2022, 2, 1);
+        TimeSpan difference = DateTimeUtils.GetDateDifference(firstDate, secondDate);
+        Console.WriteLine($"Разница между {firstDate} и {secondDate}: {difference.TotalDays} дней");
+
+        int year = 2024;
+        bool isLeapYear = DateTimeUtils.IsLeapYear(year);
+        Console.WriteLine($"{year} это високосный год: {isLeapYear}");
+
+        DateTime time = DateTime.Now;
+        string timeOfDay = DateTimeUtils.GetTimeOfDay(time);
+        Console.WriteLine($"Время суток в {time}: {timeOfDay}");
+
+        DateTime dateTime = DateTime.Now;
+        string formattedDateTime = DateTimeUtils.FormatDateTime(dateTime, "dd.MM.yyyy HH:mm:ss");
+        Console.WriteLine($"Отформатированные дата и время: {formattedDateTime}");
+    }
+}
